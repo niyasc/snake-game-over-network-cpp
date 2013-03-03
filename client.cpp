bce891746268df127854c5fb08d35b40c93c365e
niyasc <niyasmonc@gmail.com>
2013-03-03 10:57:19 +0530

    attempt to transfer data structures

--- client.cpp
+++ client.cpp
@@ -3,14 +3,188 @@
 #include<unistd.h>
 #include<stdlib.h>
 #include<arpa/inet.h>
+#include<math.h>
+#define R 10
+#define FOOD 1
+#define winWidth 500
+#define winHeight 200
 using namespace std;
+class Color
+{
+	public :
+		float r,g,b;
+};
+class Food
+{
+	public:
+		Color color;
+		int x,y;
+		void Generate()
+		{
+			color.r=(float)((random()%4)/4.0);
+			color.g=(float)((random()%4)/4.0);
+			color.b=(float)((random()%4)/4.0);
+			x=random()%winWidth;
+			y=random()%winHeight;
+		}
+		Food()
+		{
+			color.r=(float)((random()%4)/4.0);
+			color.g=(float)((random()%4)/4.0);
+			color.b=(float)((random()%4)/4.0);
+			x=random()%winWidth;
+			y=random()%winHeight;
+		}
+}f[FOOD];
+class Point
+{
+	public:
+		int x,y;
+		Point *next;
+		Point(int x,int y)
+		{
+			Point::x=x;
+			Point::y=y;
+			next=NULL;
+		}
+};
+class Snake
+{
+	public:
+		//char *name;
+		char name[20];
+		int score;
+		Point *list;
+		Color color;
+		int dir_x,dir_y;		
+		Snake();
+		~Snake();
+		void Draw();
+		void Update();
+		friend ostream& operator<<(ostream &mycout,Snake s);
+}snakes[5];
+ostream &operator<<(ostream &mycout,Snake s)
+{
+	cout<<endl;
+	cout<<"Name\t"<<s.name<<endl;
+	cout<<"Score\t"<<s.score<<endl;
+	cout<<"Cordinates\t"<<endl;
+	/*Point *temp=s.list;
+	while(temp->next!=NULL)
+	{
+		cout<<"("<<temp->x<<","<<temp->y<<"),";
+		temp=temp->next;
+	}*/
+	//cout<<"("<<temp->x<<","<<temp->y<<")"<<endl;;
+	cout<<"Color\t"<<"("<<s.color.r<<","<<s.color.g<<","<<s.color.b<<")"<<endl;
+	cout<<"Direction\t"<<s.dir_x<<","<<s.dir_y<<endl;
+	return mycout;
+}
+	
+Snake::Snake()
+{
+	int x,y;
+	score=0;
+	dir_x=0;
+	dir_y=0;
+	srandom(random()%500);
+	do
+	{
+		x=random()%500;
+		y=random()%500;
+	}while(x<=100||y<=100);
+	switch(random()%4)
+	{
+		case 0:dir_x=+1;
+			//color.r==color.g=
+			break;
+		case 1:dir_x=-1;
+			break;
+		case 2:dir_y=+1;
+			break;
+		case 3:dir_y=-1;
+			break;
+	}
+	color.r=(float)((random()%4)/4.0);
+	color.g=(float)((random()%4)/4.0);
+	color.b=(float)((random()%4)/4.0);
+	Point *t=new Point(x,y);
+	list=t;
+	for(int i=0;i<10;i++)
+	{
+		x+=R*1.6*dir_x;
+		y+=R*1.6*dir_y;
+		Point *p=new Point(x,y);
+		t->next=p;
+		t=p;
+	}
+}
+Snake::~Snake()
+{
+	Point *t=list;
+	while(t->next!=NULL)
+	{
+		Point *p=t;
+		t=t->next;
+		delete p;
+	}
+	delete t;
+}
+		
+void Snake::Update()
+{
+	Point *t;
+	t=list;
+	list=list->next;
+	free(t);
+	t=list;
+	while(t->next!=NULL)
+		t=t->next;
+	Point *p=new Point(t->x+dir_x*R*1.6,t->y+dir_y*R*1.6);
+	t->next=p;
+	t=t->next;
+	if(t->x>=winWidth)
+		t->x-=winWidth;
+	if(t->y>=winHeight)
+		t->y-=winHeight;
+	if(t->x<=0)
+		t->x+=winWidth;
+	if(t->y<=0)
+		t->y+=winHeight;
+	//do touch food?
+	for(int i=0;i<FOOD;i++)
+	{
+		if(sqrt((f[i].x-t->x)*(f[i].x-t->x)+(f[i].y-t->y)*(f[i].y-t->y))<2*R)
+		{
+			f[i].Generate();
+			score+=10;
+			if(score%30==0)
+			{
+				t=list;
+				while(t->next!=NULL)
+					t=t->next;
+				Point *p=new Point(t->x+dir_x*R*1.6,t->y+dir_y*R*1.6);
+				t->next=p;
+				t=t->next;
+				if(t->x>=winWidth)
+					t->x-=winWidth;
+				if(t->y>=winHeight)
+					t->y-=winHeight;
+				if(t->x<=0)
+					t->x+=winWidth;
+				if(t->y<=0)
+					t->y+=winHeight;
+			}	
+		}
+	}
+}
 int main()
 {
 	int sockfd;
 	int len;
 	sockaddr_in address;
 	int result;
-	int d;
+	int count;
 	
 	sockfd=socket(AF_INET,SOCK_STREAM,0);
 	
@@ -25,12 +199,19 @@ int main()
 		cout<<"Failed to connect"<<endl;
 		exit(1);
 	}
-	//write(sockfd,&ch,1);
-	while(1)
+	cout<<"Going to read socket"<<endl;
+	read(sockfd,&count,sizeof(int));
+	cout<<"Number of clients = "<<count;
+	for(int i=0;i<count;i++)
 	{
-		read(sockfd,&d,sizeof(int));
-		cout<<"Server says "<<d<<endl;
-	}
+		write(sockfd,snakes[i].name,sizeof(snakes[i].name));
+		write(sockfd,&snakes[i].score,sizeof(snakes[i].score));
+		write(sockfd,&snakes[i].color,sizeof(snakes[i].color));
+		write(sockfd,&snakes[i].dir_x,sizeof(snakes[i].dir_x));
+		write(sockfd,&snakes[i].dir_y,sizeof(snakes[i].dir_y));
+		cout<<snakes[i];
+		cout<<"out of cout"<<endl;
+	}	
 	close(sockfd);
 	exit(0);
 }
--- server.cpp
+++ server.cpp
@@ -6,12 +6,166 @@
 #include<unistd.h> //fork
 #include<netinet/in.h> //accept
 #include<pthread.h> //multithreading
+#include<math.h>
+#define winWidth 500
+#define winHeight 200
+#define CLIENTS 3
+#define R 10
+#define FOOD 1
 using namespace std;
-void *serve(void *);
-int id=0;
-int a=0;
-int chance=0;
-int count;
+class Color
+{
+	public :
+		float r,g,b;
+};
+class Food
+{
+	public:
+		Color color;
+		int x,y;
+		void Generate()
+		{
+			color.r=(float)((random()%4)/4.0);
+			color.g=(float)((random()%4)/4.0);
+			color.b=(float)((random()%4)/4.0);
+			x=random()%winWidth;
+			y=random()%winHeight;
+		}
+		Food()
+		{
+			color.r=(float)((random()%4)/4.0);
+			color.g=(float)((random()%4)/4.0);
+			color.b=(float)((random()%4)/4.0);
+			x=random()%winWidth;
+			y=random()%winHeight;
+		}
+}f[FOOD];
+class Point
+{
+	public:
+		int x,y;
+		Point *next;
+		Point(int x,int y)
+		{
+			Point::x=x;
+			Point::y=y;
+			next=NULL;
+		}
+};
+class Snake
+{
+	public:
+		//char *name;
+		char name[20];
+		int score;
+		Point *list;
+		Color color;
+		int dir_x,dir_y;		
+		Snake();
+		~Snake();
+		void Draw();
+		void Update();
+}snakes[5];
+Snake::Snake()
+{
+	int x,y;
+	score=0;
+	dir_x=0;
+	dir_y=0;
+	srandom(random()%500);
+	do
+	{
+		x=random()%500;
+		y=random()%500;
+	}while(x<=100||y<=100);
+	switch(random()%4)
+	{
+		case 0:dir_x=+1;
+			//color.r==color.g=
+			break;
+		case 1:dir_x=-1;
+			break;
+		case 2:dir_y=+1;
+			break;
+		case 3:dir_y=-1;
+			break;
+	}
+	color.r=(float)((random()%4)/4.0);
+	color.g=(float)((random()%4)/4.0);
+	color.b=(float)((random()%4)/4.0);
+	Point *t=new Point(x,y);
+	list=t;
+	for(int i=0;i<10;i++)
+	{
+		x+=R*1.6*dir_x;
+		y+=R*1.6*dir_y;
+		Point *p=new Point(x,y);
+		t->next=p;
+		t=p;
+	}
+}
+Snake::~Snake()
+{
+	Point *t=list;
+	while(t->next!=NULL)
+	{
+		Point *p=t;
+		t=t->next;
+		delete p;
+	}
+	delete t;
+}			
+		
+void Snake::Update()
+{
+	Point *t;
+	t=list;
+	list=list->next;
+	free(t);
+	t=list;
+	while(t->next!=NULL)
+		t=t->next;
+	Point *p=new Point(t->x+dir_x*R*1.6,t->y+dir_y*R*1.6);
+	t->next=p;
+	t=t->next;
+	if(t->x>=winWidth)
+		t->x-=winWidth;
+	if(t->y>=winHeight)
+		t->y-=winHeight;
+	if(t->x<=0)
+		t->x+=winWidth;
+	if(t->y<=0)
+		t->y+=winHeight;
+	//do touch food?
+	for(int i=0;i<FOOD;i++)
+	{
+		if(sqrt((f[i].x-t->x)*(f[i].x-t->x)+(f[i].y-t->y)*(f[i].y-t->y))<2*R)
+		{
+			f[i].Generate();
+			score+=10;
+			if(score%30==0)
+			{
+				t=list;
+				while(t->next!=NULL)
+					t=t->next;
+				Point *p=new Point(t->x+dir_x*R*1.6,t->y+dir_y*R*1.6);
+				t->next=p;
+				t=t->next;
+				if(t->x>=winWidth)
+					t->x-=winWidth;
+				if(t->y>=winHeight)
+					t->y-=winHeight;
+				if(t->x<=0)
+					t->x+=winWidth;
+				if(t->y<=0)
+					t->y+=winHeight;
+			}	
+		}
+	}
+}
+void *serve(void *);  //client serving function prototype
+int id=0;//client id
+int count=0;//Number of clients connected
 int main()
 {
 	int server_sockfd,client_sockfd;
@@ -19,8 +173,6 @@ int main()
 	sockaddr_in server_address;
 	sockaddr_in client_address;
 	pthread_t tid;
-	cout<<"Enter number of clients "<<endl;
-	cin>>count;
 	server_sockfd=socket(AF_INET,SOCK_STREAM,0);
 	
 	server_address.sin_family=AF_INET;
@@ -40,49 +192,37 @@ int main()
 		
 		client_len=sizeof(client_address);
 		client_sockfd=accept(server_sockfd,(struct sockaddr *)&client_address,&client_len);
-		count--;
+		count++;
 		if(pthread_create(&tid, NULL,serve, &client_sockfd)!=0)
 		{
 			cout<<"Thread error";
 			exit(1);
 		}
-		//count++;
-		//cout<<"Waiting for "<<(3-count)<<"more clients "<<endl;
-		/*if(clone(()==0)
-		{
-			while(1)
-			{
-				sleep(1);
-				a++;
-				write(client_sockfd,&a,sizeof(a));
-			}
-			close(client_sockfd);
-			exit(0);
-		}
-		else
-		{
-			close(client_sockfd);
-		}*/
 	}
 	return 0;
 }
 void *serve(void *arg)
 {
-	int id=::id;
+	static int id=::id;
 	int client_sockfd = *((int*)arg);
-	cout<<"my id is "<<id;
 	::id++;
-	while(count)
+	while(count<CLIENTS)
 	{
-		cout<<"\r Waiting for "<<count<<"clients";
+		cout<<"\r Waiting for "<<(3-count)<<"clients";
 	}
 	sleep(1);
-	while(1)
+	cout<<"Number of clients= "<<count<<endl;
+	write(client_sockfd,&count,sizeof(count));
+	for(int i=0;i<CLIENTS;i++)
 	{
-		sleep(1);
-		if(!id)
-			a++;
-		write(client_sockfd,&a,sizeof(a));
+		//write(client_sockfd,&id,sizeof(d));
+		//write(client_sockfd,&snakes[i],sizeof(snakes[id]));
+		write(client_sockfd,&snakes[i].name,sizeof(snakes[i].name));
+		write(client_sockfd,&snakes[i].score,sizeof(snakes[i].score));
+		write(client_sockfd,&snakes[i].color,sizeof(snakes[i].color));
+		write(client_sockfd,&snakes[i].dir_x,sizeof(snakes[i].dir_x));
+		write(client_sockfd,&snakes[i].dir_y,sizeof(snakes[i].dir_y));
 	}
 	close(client_sockfd);
+	pthread_exit(arg);
 }
